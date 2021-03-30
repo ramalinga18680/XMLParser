cc33cc384e226d3f6a5a4baf1b1bf1e399c09287
vishnu.tvrprasad <vishnu.tvrprasad@gmail.com>
2021-03-30 17:22:18 -0400

    Added logic to write row using append of work sheet

--- parser4.py
+++ parser4.py
@@ -1,9 +1,112 @@
 from lxml import etree
 import sys
+import openpyxl
+
+
+class Analyzer():
+    def __init__(self):
+        self.test_case = ""
+        self.ts_case_obj = None
+        self.report_ws = None
+        self.row_to_write = 2
+    '''
+
+    def writerow(self, data, row_to_write):
+        i = 0
+        max_range: int = 1 + len(data)
+        for col in range(1, max_range):
+            self.report_ws.cell(row_to_write, col).value = data[i]
+            i += 1
+    '''
+    def writerow(self, data,row_to_write=None):
+        self.report_ws.append(data)
+
+    def write_header(self):
+
+        if self.report_ws is not None:
+            data = ["Test Pack", "Test Case Id", "Test Result", "Total Steps", "Total Steps Passed",
+                    "Total Steps Failed", "Has Ignition", "Ignition Status", "Has Audio Verification",
+                    "Audio Verification Status"]
+            self.writerow(data, 1)
+
+    def initialize(self, report):
+        self.report_ws = report
+        self.write_header()
+
+    def analyze(self, test_case):
+        print("Analyzing the test case")
+        self.ts_case_obj = TestCase(test_case, self)
+        self.ts_case_obj.process_test_steps()
+        self.ts_case_obj.process_func_units()
+        self.generate_report()
+
+        # list_func_units = ts_case.xpath('./functionUnit[functionUnitName[contains(text(),"Ignition")]]')
+        del self.ts_case_obj
+        self.ts_case_obj = None
+
+    def generate_report(self):
+        data = []
+        if self.report_ws is not None:
+            data.append(self.ts_case_obj.find_test_pack_details())
+            data.append(self.ts_case_obj.find_test_caseid())
+            data.append(self.ts_case_obj.find_test_case_result())
+            data.append(self.ts_case_obj.num_total_steps)
+            data.append(self.ts_case_obj.num_passed_test_steps)
+            data.append(self.ts_case_obj.num_failed_test_steps)
+
+            if self.ts_case_obj.has_ignition:
+                data.append("Yes")
+                if self.ts_case_obj.ignition_failure is True:
+                    data.append("Fail")
+                else:
+                    data.append("Pass")
+            else:
+                data.append("No")
+                data.append("NA")
+
+            if self.ts_case_obj.has_audio_verification:
+                data.append("Yes")
+                if self.ts_case_obj.verify_audio_failure is True:
+                    data.append("Fail")
+                else:
+                    data.append("Pass")
+            else:
+                data.append("No")
+                data.append("NA")
+
+            #self.writerow(data, self.row_to_write)
+            self.writerow(data)
+            self.row_to_write += 1
+        print("test_pack : tc_id : tc_req_id : tc_result", self.ts_case_obj.find_test_caseid(),
+              self.ts_case_obj.find_test_case_reqid(), self.ts_case_obj.find_test_case_result(),
+              self.ts_case_obj.find_test_pack_details())
+        print(" Total steps :%d passed steps : %d Failed test steps :%d has ignition failure %d" % (
+            self.ts_case_obj.num_total_steps, self.ts_case_obj.num_passed_test_steps,
+            self.ts_case_obj.num_failed_test_steps, self.ts_case_obj.ignition_failure))
+        if self.ts_case_obj.verify_audio_failure and self.ts_case_obj.has_audio_verification:
+            print("Audio verification failed")
+
+    def on_test_step_found(self, elem_ts_cmd, elem_ts_result):
+        if "VERIFY_AUDIO" in elem_ts_cmd.text:
+            self.ts_case_obj.has_audio_verification = True
+            if elem_ts_result.text == "FAILURE":
+                self.ts_case_obj.verify_audio_failure = True
+
+        if elem_ts_result.text == "SUCCESS":
+            self.ts_case_obj.num_passed_test_steps += 1
+        elif elem_ts_result.text == "FAILURE":
+            self.ts_case_obj.num_failed_test_steps += 1
+
+    def on_func_unit_found(self, func_unit_name, func_unit_result):
+        if "Ignition" in func_unit_name.text:
+            self.ts_case_obj.has_ignition = True
+            # print("Func unit name ", func_unit_name.text, func_unit_result.text)
+            if func_unit_result.text == "FAILURE":
+                self.ts_case_obj.ignition_failure = True
 
 
 class TestCase():
-    def __init__(self, ts_case_element, out=sys.stdout):
+    def __init__(self, ts_case_element, analyzer, out=sys.stdout):
         self.req_id = ""
         self.test_case_id = ""
         self.result = ""
@@ -12,11 +115,13 @@ class TestCase():
         self.num_total_steps = 0
         self.num_passed_test_steps = 0
         self.num_failed_test_steps = 0
+        self.has_ignition = False
         self.ignition_failure = False
+        self.has_audio_verification = False
         self.verify_audio_failure = False
+        self.analyzer = analyzer
 
-    #def __del__(self):
-
+    # def __del__(self):
 
     def find_test_caseid(self):
         elem_test_case_id = self.ts_case_element.find("./testCaseID")
@@ -38,23 +143,13 @@ class TestCase():
         list_test_steps = self.ts_case_element.findall(".//TestStep")
         return list_test_steps
 
-    def check_if_audio_failure(self, test_step):
-        ts_command = test_step.find("./command").text
-        # print("ts_command is",ts_command)
-        if "VERIFY_AUDIO" in ts_command:
-            return True
-        return False
-
     def process_test_steps(self):
         list_steps = self.find_test_steps()
         self.num_total_steps = len(list_steps)
         for ts in list_steps:
             ts_result = ts.find("./result")
-            if ts_result.text == "SUCCESS":
-                ts_case_obj.num_passed_test_steps += 1
-            elif ts_result.text == "FAILURE":
-                ts_case_obj.num_failed_test_steps += 1
-                self.verify_audio_failure = self.check_if_audio_failure(ts)
+            ts_command = ts.find("./command")
+            self.analyzer.on_test_step_found(ts_command, ts_result)
 
     def find_function_units(self):
         list_function_units = self.ts_case_element.findall("./functionUnit")
@@ -67,34 +162,23 @@ class TestCase():
         for f_unit in list_f_units:
             f_uname = f_unit.find(elements[0])
             f_result = f_unit.find(elements[1])
-            self.check_if_func_unit_has_ignition(f_uname, f_result)
-
-    def check_if_func_unit_has_ignition(self, func_unit_name, func_unit_result):
-        if "Ignition" in func_unit_name.text:
-            print("Func unit name ", func_unit_name.text, func_unit_result.text)
-            if func_unit_result.text == "FAILURE":
-                self.ignition_failure = True
+            self.analyzer.on_func_unit_found(f_uname, f_result)
 
 
 if __name__ == '__main__':
     tree = etree.parse(sys.argv[1])
 
+    report_wb = openpyxl.Workbook();
+    report_sheet = report_wb.create_sheet(title='Analysis', index=0)
+
+    analyzer_obj = Analyzer();
+    analyzer_obj.initialize(report_sheet)
+
     list_test_case = tree.findall(".//TestCase")
     print("Num of test cases is", len(list_test_case))
 
     for ts_case in list_test_case:
-        ts_case_obj = TestCase(ts_case)
-        ts_case_obj.process_test_steps()
-        ts_case_obj.process_func_units()
+        analyzer_obj.analyze(ts_case)
 
-        print("test_pack : tc_id : tc_req_id : tc_result", ts_case_obj.find_test_caseid(),
-              ts_case_obj.find_test_case_reqid(), ts_case_obj.find_test_case_result(),
-              ts_case_obj.find_test_pack_details())
-        print(" Total steps :%d passed steps : %d Failed test steps :%d has ignition failure %d" % (
-            ts_case_obj.num_total_steps, ts_case_obj.num_passed_test_steps,
-            ts_case_obj.num_failed_test_steps, ts_case_obj.ignition_failure))
-        if ts_case_obj.verify_audio_failure:
-            print("Audio verification failed")
-
-        # list_func_units = ts_case.xpath('./functionUnit[functionUnitName[contains(text(),"Ignition")]]')
-        del ts_case_obj
+    report_wb.save(sys.argv[2])
+    report_wb.close()
